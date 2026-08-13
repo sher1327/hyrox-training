18b437a6c81eeec860fb8bf84bea525ba7c2c111
sher1327 <83964878+sher1327@users.noreply.github.com>
2026-08-13 12:19:02 +0800

    Apply plugin compile SDK override after evaluation

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,9 +19,11 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
-    plugins.withId("com.android.library") {
-        extensions.configure<com.android.build.api.dsl.LibraryExtension> {
-            compileSdk = 36
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.library")) {
+            extensions.configure<com.android.build.api.dsl.LibraryExtension> {
+                compileSdk = 36
+            }
         }
     }
 }
